9590fe41e707bcfe87d905dd8307317e40918b0d
taxio <taxio.2029@gmail.com>
2020-05-11 08:37:32 +0900

    implemented url patterns additional function

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,28 +1,41 @@
 import React, { useState, useEffect } from 'react';
 
+const KeyUrlPatterns: string = "url_patterns";
+
 const defaultUrlPatterns: Array<string> = [
   "chrome://newtab/", 
   "https://www.google.com/search"
 ];
 
 const Settings: React.FC = () => {
-  const [urlPatterns, setUrlPatterns] = useState<Array<string>>(defaultUrlPatterns);
+  const [urlPatterns, setUrlPatterns] = useState<Array<string>>([]);
   const [url, setUrl] = useState<string>('');
 
   const addUrlPatterns = () => {
     setUrlPatterns([...urlPatterns, url]);
+    setUrl('');
   };
 
   useEffect(() => {
-    // TODO: load URLs from storage
-    // TODO: set URLs
-    // TODO: save URLs to storage
-  });
+    chrome.storage.sync.get(KeyUrlPatterns, (items) => {
+      console.log(items);
+      if (items.hasOwnProperty(KeyUrlPatterns)) {
+        setUrlPatterns(items[KeyUrlPatterns]);
+      } else {
+        setUrlPatterns(defaultUrlPatterns);
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    console.log("useEffect: urlPatterns");
+    chrome.storage.sync.set({KeyUrlPatterns: urlPatterns});
+  }, [urlPatterns]);
 
   return (
     <div>
-    { urlPatterns.map((pt: string) => (
-      <p>{pt}</p>
+    { urlPatterns.map((pt: string, index: number) => (
+      <p key={index}>{pt}</p>
     )) }
     <input type="text" value={url} onChange={e => setUrl(e.target.value)} />
     <button onClick={addUrlPatterns}>save</button>
