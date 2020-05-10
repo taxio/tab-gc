eed7958b813609967b8ed038a89499a63d552d6b
taxio <taxio.2029@gmail.com>
2020-05-11 04:53:21 +0900

    configure options page using React

---
+++ src/options.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+
+ReactDom.render(
+  <React.StrictMode>
+    <p>Options Page</p>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
